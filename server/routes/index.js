6fb0d9bded460da69c27334397ac5d245b868cea
Mikel Severson <mkseve@gmail.com>
2016-04-02 19:47:22 -0500

    new endpoint

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,8 +4,8 @@ var router = express.Router();
 
 router.get("/endpoint", (req, res, next) => {
 	res.json({
-		'title': 'How do you get people to notice you online?',
-		'message': 'You have to really make an impression',
+		'title': 'AdWords? More like...',
+		'message': 'AdDon’tgettoowordybecauseyouonlyhave70characters',
 		'link': 'https://www.mikelseverson.com',
 		'image' : {
 			'url': 'https://cdn2.iconfinder.com/data/icons/advertisement-marketing/512/ad_banner-512.png'
