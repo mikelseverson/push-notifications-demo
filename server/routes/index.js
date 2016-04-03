78ea17bec72abc62ff151fc3958b5c6d3d6fb0f1
Mikel Severson <mkseve@gmail.com>
2016-04-02 19:32:37 -0500

    updates endpoint

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,8 +4,8 @@ var router = express.Router();
 
 router.get("/endpoint", (req, res, next) => {
 	res.json({
-		'title': 'Why did the market get off the trampoline',
-		'message': 'Gotta keep the bounce rate down',
+		'title': 'Why marketer get off trampoline?',
+		'message': 'Bounce rate was too high',
 		'link': 'https://www.mikelseverson.com',
 		'image' : {
 			'url': 'https://cdn2.iconfinder.com/data/icons/advertisement-marketing/512/ad_banner-512.png'
